0b94e0e4b341df4aefe2be7003fb3a71260a0c1c
github username <github email address>
2019-11-22 23:11:40 +0000

    Done.

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -12,33 +12,27 @@ class GifListContainer extends React.Component {
     componentDidMount() {
         fetch(`https://api.giphy.com/v1/gifs/search?q=dolphin&api_key=RmX6ryJ4LGv3hP6WEug3pSstURBoGjFh&rating=g`)
             .then(resp => resp.json())
-            .then(gifs => {
-                console.log(gifs)
+            .then(gifs => this.selectGifs(gifs))
+    }
+                
+    selectGifs = (gifs) => {
+        console.log(gifs)
             let urls = []
-            urls.push(gifs.data[0].images.original.url)
-            urls.push(gifs.data[1].images.original.url)
-            urls.push(gifs.data[2].images.original.url)
+            for (let i=0; i<3; i++) {
+                urls.push(gifs.data[i].images.original.url)
+            }
             this.setState({
                 gifsurls: urls
                 })
-           })
     }
 
     getGifsData = (searchword) => {
        
         fetch(`https://api.giphy.com/v1/gifs/search?q=${searchword}&api_key=RmX6ryJ4LGv3hP6WEug3pSstURBoGjFh&rating=g`)
         .then(resp => resp.json())
-        .then(gifs => {
-            console.log(gifs)
-        let urls = []
-        urls.push(gifs.data[0].images.original.url)
-        urls.push(gifs.data[1].images.original.url)
-        urls.push(gifs.data[2].images.original.url)
-        this.setState({
-            gifsurls: urls
-            })
-       })
+        .then(gifs => this.selectGifs(gifs))
     }
+
 
     render (){
         
